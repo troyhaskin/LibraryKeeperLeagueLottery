4c5d9283e1da8821bb344873636d5099b50bc850
Troy Haskin <git.troy@hask.in>
2015-01-13 20:00:51 -0600

    More HTML/jQuery expansion.

--- Application.js
+++ Application.js
@@ -2,36 +2,73 @@
 
     "use strict";
     
-    var theLibraryKeeper  = League;
-    var theLibraryLottery = Lottery;
+    var theLibraryKeeper  = Object.create(League);
+    var theLibraryLottery = Object.create(Lottery);
     
     (function ($){
+    
+        // Margin adjustments for the displayed headers
+        var reMarginDiv = function (id) {
+            var element = $("div#InputHeaders > div#"+id),
+                labelLeft = $(element).offset().left,
+                inputLeft = $("div#FirstTeam > input[name="+id+"]").offset().left;
+            
+            $(element).css("margin-left",inputLeft-labelLeft);
+            
+        };
+        var labels = ["TeamName","DraftPick","InLottery"];
+        labels.forEach ( function (id) {
+            reMarginDiv(id);
+        });
+        
     
     
         var checkedCount = 0;
         $("[name='InLottery']").click(function (event) {
-            $(this).siblings("[name='PickChance']").toggle();
+            var InLotteryCheckbox = this;
+            
+            $(this).siblings("[name='PickChance']").toggle(100, function () {
+                if ($(InLotteryCheckbox).is(":checked")) {
+                    $(InLotteryCheckbox).siblings("[name='DraftPick']").attr("disabled","disabled");
+                    $(InLotteryCheckbox).siblings("[name='DraftPick']").css("background-color","#dddddd");
+                    checkedCount += 1;
+                } else {
+                    $(InLotteryCheckbox).siblings("[name='DraftPick']").removeAttr("disabled");
+                    $(InLotteryCheckbox).siblings("[name='DraftPick']").css("background-color","#ffffff");
+                    checkedCount -= 1;
+                }
 
-            if ($(this).is(":checked")) {
-                $(this).siblings("[name='DraftPick']").attr("disabled","disabled");
-                $(this).siblings("[name='DraftPick']").css("background-color","#dddddd");
-                checkedCount += 1;
-            } else {
-                $(this).siblings("[name='DraftPick']").removeAttr("disabled");
-                $(this).siblings("[name='DraftPick']").css("background-color","#ffffff");
-                checkedCount -= 1;
-            }
-                                    
-            if (checkedCount === 1) {
-                $("input#PerformLottery").show(100);
-            }
+                if (checkedCount === 1) {
+                
+                    // Show PickChance column and resize once
+                    $("div#PickChance").show(100, function () {
+                        if (undefined !== reMarginDiv) {
+                            reMarginDiv("PickChance");
+                            reMarginDiv = function (){}; // We're done with this function
+                        }
+                    });
 
-            if (0 >= checkedCount) {
-                $("input#PerformLottery").hide(100);
-            }
+                    
+                    // Activate draft 
+                    $("input#PerformLottery").show(100);
+                    $("div#DraftColumn").css("pointer-events","");
+                    $("div#DraftColumn > div.ColumnHeader").css("opacity",1);
+                    
+                }
+
+                if (0 >= checkedCount) {
+
+                    $("div#PickChance").hide(100);
+                    
+                    // Deactivate draft 
+                    $("input#PerformLottery").hide(100);
+                    $("div#DraftColumn").css("pointer-events","none");
+                    $("div#DraftColumn > div.ColumnHeader").css("opacity",0.1);
+                }
+            });
         });
         
-        $("[name='Name']").blur(function (event) {
+        $("[name='TeamName']").blur(function (event) {
             var TeamName   = $(this).val().trim(),
                 PickChance = $(this).siblings("[name='PickChance']").val();
             
@@ -47,11 +84,14 @@
             $(this).parent().remove();
         });
         
-        $("input#AddNewTeam").click( function (event) {
+        var inLotteryWidth = $("div#InLottery").width();
+        $("[name='AddTeam']").click( function (event) {
             var newTeam = $("div#BlankTeam").clone(true);
+            newTeam.siblings("[name='InLottery']").width(inLotteryWidth);
             $("div#League").append(newTeam.attr("id",""));
             newTeam.toggle(100);
         });
+        
         
         
         
